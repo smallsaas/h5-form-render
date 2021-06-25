c928b005db9700e924a74110da581206caa72140
ruohan <1534007973@qq.com>
2021-06-25 18:30:17 +0800

    feat: 全局配置修改

--- config.js
+++ config.js
@@ -5,7 +5,11 @@ const globalConfigStorage = uni.getStorageSync('globalConfigStorage')
 
 export const globalConfig = {
   host: 'https://xxx',
+  useCache: globalConfigStorage.useCache || false,
   formHost: _.get(globalConfigStorage, 'formEP', 'https://api.mock.smallsaas.cn/form'),
   dataHost: _.get(globalConfigStorage, 'dataEP', 'https://api.mock.smallsaas.cn/data'),
+  endpoint: _.get(globalConfigStorage, 'endpoint', 'https://api.cloud.smallsaas.cn'),
+  workflow: _.get(globalConfigStorage, 'workflow', 'https://api.wf.smallsaas.cn'),
+  authEP: _.get(globalConfigStorage, 'authEP', 'https://api.uaas.smallsaas.cn'),
   tokenStorageKey: 'token'  //tokené”
 }
